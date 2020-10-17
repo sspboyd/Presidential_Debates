9739ad90b2c8ddb0c8ea7aa5f9eff588f9f388f8
sspboyd <stephen@sspboyd.ca>
2020-10-17 00:23:46 -0400

    Converted p5 code to instance mode. And rewrote from original prototype.

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,136 +1,131 @@
 // quick outline of a way to view the interuptions in the first (and
 // now maybe only) Presidential debate.
 
-// const lines = 600;
-// const transcript = Array(lines).fill(0);
 const speakers = {
   "President Donald J. Trump": {
-    name: "President Donald J. Trump",
+    full_name: "President Donald J. Trump",
+    name: "Trump",
     clr: "#E91D0E",
     dir: -1,
     orig_X: 500
   },
   "Chris Wallace": {
-    name: "Chris Wallace",
+    full_name: "Chris Wallace",
+    name: "Wallace",
     clr: "#AAA",
     dir: 0,
     orig_X: 250
 
   },
   "Vice President Joe Biden": {
-    name: "Vice President Joe Biden",
+    full_name: "Vice President Joe Biden",
+    name: "Biden",
     clr: "#232066",
     dir: 1,
     orig_X: 0
   }
 };
 
-// const new_transcript = transcript.map((t) => {
-//   return {
-//     name: function() {
-//       let names = Object.keys(speakers);
-//       // console.log(names)
-//       let curr_name = names[Math.floor(names.length * Math.random())];
-//       // console.log(curr_name);
-//       return curr_name;
-//     }(),
-//     text: "I am talking",
-//     time: "00:33:22",
-//     text_len: Math.floor(Math.random() * Math.floor(321))
-//   };
-// });
-
-// new_transcript.map((t) => {
-//   // return console.log(t.text_len);
-// });
-
-// Globals. yck
-let transcript_data;
-let lineH = 7;
-
-
-function preload(){
-  let url = '../newTranscript.json';
-  transcript_data = loadJSON(url);
+const titles = {
+  main_title: "Visualizing the Presidential Debate",
+  sub_title: "The first and maybe only debate. \nTuesday September 29, 2020.",
+  description: "Each line represents either Joe Biden, Donald Trump or Chris Wallace talking. The length of each line is determined by how many words were spoken.",
+  credit: "Original transcription from Rev transcription services.",
+  author: "Built by Stephen Boyd, @sspboyd",
 }
 
-function setup() {
-  createCanvas(1000, 10000);
-  background(255);
-  noLoop();
-  // console.trace(transcript_data)
-  noStroke();
-  for(i in transcript_data){
-    let px = 0;
-    curr_line = transcript_data[i];
-    let curr_line_wc = curr_line["Word Count"];
-    let pw = map(curr_line_wc, 0,199,0,width/2)
+const s = (p55) => {
 
-    let speaker_clr = speakers[curr_line.Name].clr;
-    // console.table({curr_line});
-    push();
-    if (curr_line.Name === "President Donald J. Trump") {
-      px = width / 2;
-    } else if (curr_line.Name === "Vice President Joe Biden") {
-      px = width / 2;
-      pw=pw * -1;
-    } else if (curr_line.Name === "Chris Wallace") {
-      px = width/2-(pw/2);
-    }
-    translate(px, (lineH + 1) * i);
-    fill(speaker_clr);
-    console.log({px, pw, lineH, speaker_clr});
-    rect(0, 0, pw, lineH,lineH/3)
-    pop();
+  let transcript_data;
+  let lineH = 7;
+
+  p55.preload = () => {
+    let url = '../newTranscript.json';
+    transcript_data = p55.loadJSON(url);
   };
 
-  // for (let i = 0; i < transcript_data.length; i++) {
-  // }
+  const transcript_entry = {
+    init: function () {
+      // maybe call to calculate initial values for things?
+      // set w, h, curr_loc, clr?
+    },
+    update: function () {
+      console.log("I'm an update function!");
+    },
+    render: function () {
+      console.log("I'm the render function");
+    },
+    curr_loc: p55.createVector(),
+    prev_loc: p55.createVector(),
+    h: lineH,
+    w: 0,
+    full_name: "",
+    name: "",
+  };
 
-  // stroke(0);
-  // strokeWeight(.3);
-  // noFill();
-  // noStroke();
+  const create_transcript_entry = t => {
+    let entry = Object.create(t);
+    return entry;
+  };
 
-  // beginShape();
-  // curveVertex(width / 2, 0);
-  // curveVertex(width / 2, 0);
-
-
-//   for (let i = 0; i < new_transcript.length; i++) {
-//     let px = 0;
-//     let py = 0;
-//     curr_line = new_transcript[i];
-//     // curveVertex(random(200), (lineH + 1) * i);
-//     if (curr_line.name === "Trump") {
-//       px = width / 2;
-//     } else if (curr_line.name === "Biden") {
-//       px = width / 2;
-//       console.log("Biden's text_len: " + curr_line.text_len);
-//       curr_line.text_len = curr_line.text_len * -1;
-//     } else if (curr_line.name === "Wallace") {
-//       px = width / 2 - curr_line.text_len / 2;
-//     }
-// //    curr_line.text_len
-//     py = (lineH - 320) * i;
-//     let speaker_clr = speakers[curr_line.name].clr
-//     fill(speaker_clr);
-//     // rect(0, 0, , lineH)
-//     beginShape();
-    
-//     curveVertex(px+curr_line.text_len, py);
-//     curveVertex(px, py);
-//     curveVertex((px+curr_line.text_len), py + lineH);
-//     curveVertex(px, (py + lineH));
-//     endShape();
-//   }
-//   // curveVertex(width / 2, height);
-//   // curveVertex(width / 2, height);
-//   // endShape();
+  let transcript_entries = [];
 
 
-}
 
-function draw() {
+  p55.setup = () => {
+    p55.createCanvas(1000, 10000);
+    p55.background(255);
+    p55.noLoop();
+    // console.trace(transcript_data)
+    p55.noStroke();
 
-}
+    // generate an array of objects for each line in the transcript
+    for (i in transcript_data) {
+      let cl = transcript_data[i]; // cl = current line in transcript_data
+      let nto = create_transcript_entry(transcript_entry); // nto = new transcription object
+
+      nto.full_name = cl.full_name;
+      nto.name = cl.name;
+      nto.timestamp = new Date(cl.time_stamp);
+      nto.text = cl.text;
+      nto.word_count = cl.word_count;
+
+      transcript_entries.push(nto);
+    };
+    // console.table(transcript_entries);
+  };
+
+  p55.draw = () => {
+    // update transcription line objs
+    for (i in transcript_entries) {
+      let cto = transcript_entries[i]; //cto = current transcription object
+      cto.w = p55.map(cto.word_count, 0, 199, 0, p55.width / 2);
+      console.log(`${i}, ${cto.name}`);
+      let speaker_clr = speakers[cto.full_name].clr;
+
+      if (cto.name === "Trump") {
+        cto.curr_loc.x = p55.width / 2;
+      } else if (cto.name === "Biden") {
+        cto.curr_loc.x = p55.width / 2;
+        cto.w = cto.w * -1;
+      } else if (cto.name === "Wallace") {
+        cto.curr_loc.x = p55.width / 2 - (cto.w / 2);
+      }
+      cto.curr_loc.y = 0;
+
+
+      // Render functions
+      p55.push();
+      p55.translate(cto.curr_loc.x, lineH * i);
+      p55.fill(speaker_clr);
+      p55.rect(0, 0, cto.w, cto.h - 1, cto.h / 3)
+      p55.pop();
+
+    }
+    // render graph
+    // render titles
+
+  };
+};
+
+let myp5 = new p5(s);

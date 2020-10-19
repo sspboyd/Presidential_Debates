8b19eec9061acf4d495020163bf836dcd2b118c4
sspboyd <stephen@sspboyd.ca>
2020-10-19 15:25:53 -0400

    Horiz layout works. Reorg'd code too.

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,5 +1,6 @@
 // quick outline of a way to view the interuptions in the first (and
 // now maybe only) Presidential debate.
+const PHI = (Math.sqrt(5)+1)/2;
 
 const speakers = {
   "President Donald J. Trump": {
@@ -35,37 +36,24 @@ const titles = {
 }
 
 const s = (p55) => {
-  let c; // declaring the canvas var here so it can be used later in saveCanvas() func
-  let transcript_data; // holds the json data
-  let lineH = 18;
-  let transcript_entries = []; // holds the objects for each line of the transcript
-
-  let exportImg = function(){
-    let sketchName = "2020_Presidental_Debates-";
-    // generate date string like this YYYY-MM-DD-HH-MM-SS
-    let dt = new Date();
-    let datetime = `${dt.getFullYear().toString()}${(dt.getMonth()+1).toString()}${dt.getDate().toString()}${dt.getHours().toString()}${dt.getMinutes().toString()}${dt.getSeconds().toString()}`;
-    let filename = sketchName + datetime;
-    console.log(`saving canvas to: ${filename}`);
-    p55.saveCanvas(c, filename, 'png');
-  }
-
-  p55.keyTyped = () => {
-    if(p55.key === 'S'){
-      exportImg();
-    }
-  }
-
   p55.preload = () => {
     let url = '../newTranscript.json';
     transcript_data = p55.loadJSON(url);
   };
 
+  let c; // declaring the canvas var here so it can be used later in saveCanvas() func
+  let transcript_data; // holds the json data
+  let te_bar_w; // transcript entry bar width
+  let transcript_entries = []; // holds the objects for each line of the transcript
+  let canvasH = 600;
+  let canvasW = 1100;
+
+
   const transcript_entry = {
     curr_loc: p55.createVector(),
     prev_loc: p55.createVector(),
-    h: lineH,
-    w: 0,
+    h: 0,
+    w: te_bar_w,
     full_name: "",
     name: "",
     clr: "",
@@ -76,26 +64,29 @@ const s = (p55) => {
     },
 
     update: function () {
-      this.w = p55.map(this.word_count, 0, 199, 0, p55.width / 2);
-      this.h = lineH * 1;
+      this.w = te_bar_w * 1; // transcript entry bar width
+      this.h = p55.map(this.word_count, 0, 220, 0, p55.height / 2);
 
       if (this.name === "Trump") {
-        this.curr_loc.x = p55.width / 2;
+        this.curr_loc.y = p55.height / 2;
+        this.h = this.h * -1;
       } else if (this.name === "Biden") {
-        this.curr_loc.x = p55.width / 2;
-        this.w = this.w * -1;
+        this.curr_loc.y = p55.height / 2;
       } else if (this.name === "Wallace") {
-        this.curr_loc.x = p55.width / 2 - (this.w / 2);
+        this.curr_loc.y = (p55.height / 2) - (this.h / 2);
       }
-      this.curr_loc.y = 0;
+      // this.curr_loc.x = map(curr_loc.timestamp);
     },
     
     render: function () {
       p55.push();
-      p55.translate(this.curr_loc.x, lineH * i);
-      p55.noStroke();
-      p55.fill(this.clr);
-      p55.rect(0, 0, this.w, this.h - 4, this.h / 3)
+      // console.log(`i is ${i}`);
+      p55.translate(te_bar_w * i, this.curr_loc.y) // transcript entry bar width;
+      p55.stroke(this.clr);
+      p55.strokeWeight(this.w/(PHI));
+      p55.line(0,0,0,this.h);
+      // p55.fill(this.clr);
+      // p55.rect(0, 0, this.w, this.h)
       p55.pop();
     },
   };
@@ -106,8 +97,8 @@ const s = (p55) => {
   };
 
   p55.setup = () => {
-    c = p55.createCanvas(1300, 9050);
-    p55.background(255);
+    c = p55.createCanvas(canvasW, canvasH);
+    te_bar_w = canvasW/Object.keys(transcript_data).length;
 
     // generate an array of objects for each line in the transcript
     for (i in transcript_data) {
@@ -126,13 +117,33 @@ const s = (p55) => {
   };
 
   p55.draw = () => {
-    for (i in transcript_entries) {
+    p55.background(255);
+
+    for ( i in transcript_entries) {
       transcript_entries[i].update();
       transcript_entries[i].render();
     }
     // render graph
     // render titles
   };
+
+  let exportImg = function(){
+    let sketchName = "2020_Presidental_Debates-";
+    // generate date string like this YYYY-MM-DD-HH-MM-SS
+    let dt = new Date();
+    let datetime = `${dt.getFullYear().toString()}${(dt.getMonth()+1).toString()}${dt.getDate().toString()}${dt.getHours().toString()}${dt.getMinutes().toString()}${dt.getSeconds().toString()}`;
+    let filename = sketchName + datetime;
+    console.log(`saving canvas to: ${filename}`);
+    p55.saveCanvas(c, filename, 'png');
+  };
+
+  p55.keyTyped = () => {
+    if(p55.key === 'S'){
+      exportImg();
+    }
+  };
+
+
 };
 
 let myp5 = new p5(s);
